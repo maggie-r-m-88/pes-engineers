ffadf01b203d1c9778cc5f1c8a4e1b7f2c3214be
Maggie Martin <maggie.martin@digitaladditive.com>
2020-05-28 12:04:14 +0200

    adding scripts from theme options

---
+++ script.js
@@ -0,0 +1,21 @@
+(function($) {
+    
+
+/* Once page is loaded  */
+window.addEventListener("DOMContentLoaded", function(){
+   
+    $('.none-magic-line').html
+    
+    $('.related-projects-grid .vc_gitem-animated-block').removeClass('vc_gitem-animate-none');
+    $('.related-projects-grid .vc_gitem-animated-block').addClass('vc_gitem-animate-slideTop');
+});
+
+/* Prevent click on news single pretty photo / lightbox */
+$(document).ready(function() {
+	$('.single-post .entry-thumbnail_overlay').on('click', function (e) {
+    	event.preventDefault();
+  	});
+});
+
+
+})( jQuery );
